7d2051b9b35d29275f267ec64c2c1158f700afe5
Josselyn Sanchez <jsancheze9@miumg.edu.gt>
2026-08-07 15:57:29 -0600

    Cambios

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
-import { Image, Pressable, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.contenedor}>
       <Image
-        source={{ uri: "" }}
+         source={require("../../assets/images/images/Foto.jpeg")}
         style={styles.foto}
       />
       <Text style={styles.nombre}>Josselyn Sanchez</Text>
@@ -15,3 +15,49 @@ export default function App() {
     </View>
   );
 }
+const styles = StyleSheet.create({
+
+  contenedor:{
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
+    padding:16
+  },
+
+  foto:{
+    width:120,
+    height:120,
+    borderRadius:60,
+    marginBottom:16
+  },
+
+  nombre:{
+    fontSize:24,
+    fontWeight:"bold"
+  },
+
+  carrera:{
+    fontSize:16,
+    color:"#555",
+    marginTop:4
+  },
+
+  carnet:{
+    fontSize:14,
+    color:"#888",
+    marginTop:4
+  },
+
+  boton:{
+    marginTop:20,
+    backgroundColor:"#ff58b4",
+    padding:12,
+    borderRadius:8
+  },
+
+  botonTexto:{
+    color:"white",
+    fontSize:16
+  }
+
+});
